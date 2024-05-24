f999e346bd30e2cace2267582ed79f10837383fc
sam.mv74 <sam.mv74@gmail.com>
2024-05-25 00:50:42 +0330

    Initial commit

---
+++ src/test/java/com/faash/sample_rest_service/SampleRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faash.sample_rest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
